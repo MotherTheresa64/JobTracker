a39f235b27c28d3a3438fe21d2b13afa8ff59720
MotherTheresa64 <MotherTheresa646@gmail.com>
2025-07-19 16:38:48 -0500

    Final Version

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -25,7 +25,7 @@ const Navbar = () => {
           to="/"
           className="text-lg font-semibold tracking-tight flex items-center gap-2"
         >
-          🔨 <span className="text-white">TrackForge</span>
+          🔨 <span className="text-white">TaskForge</span>
         </Link>
 
         {/* Desktop Nav */}
